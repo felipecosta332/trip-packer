3aab92462cbd725f2ee858dc6e2702a5b7c45635
Felipe Costa <felipecosta776@outlook.com>
2023-08-28 18:39:37 -0300

    building the layout

--- src/App.js
+++ src/App.js
@@ -1,57 +1,34 @@
-import { useState } from "react";
-// import { DateCounterV1 } from "./DateCounterV1";
-
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
-
 export default function App() {
-  const [step, setStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(true);
-
-  function handlePrevious() {
-    setStep(step > 1 ? step - 1 : step);
-  }
-
-  function handleNext() {
-    setStep(step < 3 ? step + 1 : step);
-  }
-
   return (
-    <>
-      {/* <DateCounterV1 /> */}
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
-        &times;
-      </button>
-      {isOpen && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
-          </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
-          <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      )}
-    </>
+function Logo() {
+  return <h1>🏝️ Trip Packer 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
   );
 }
