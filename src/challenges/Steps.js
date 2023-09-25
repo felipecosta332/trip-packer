dbb8c74e5491fbdda80b4d9746ea8da78b25520b
Felipe Costa <felipecosta776@outlook.com>
2023-09-25 17:55:27 -0300

    chidren prop reusability

--- src/challenges/Steps.js
+++ src/challenges/Steps.js
@@ -21,7 +21,6 @@ export function Steps() {
 
   return (
     <>
-      {/* <DateCounterV1 /> */}
       <button className="close" onClick={() => setIsOpen(!isOpen)}>
         &times;
       </button>
@@ -32,9 +31,18 @@ export function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
 
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
@@ -42,11 +50,29 @@ export function Steps() {
             </Button>
             <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
               Next <span>👉</span>
+              <span>🤓</span>
             </Button>
           </div>
         </div>
       )}
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>✌️</p>
+      </StepMessage>
+      <StepMessage step={2}>
+        <p>Read children prop</p>
+        <p>😎</p>
+      </StepMessage>
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   );
 }
 
@@ -62,5 +88,5 @@ function Button({ textColor, bgColor, onClick, children }) {
 }
 
 /*
-👈👉🤓
+👈👉🤓✌️😎🤑💼⚛️
 */
